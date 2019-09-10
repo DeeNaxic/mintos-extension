eb3eae82107f6f45d284877a5039d7360175e789
steffen <steffen.aller@gmail.com>
2019-09-10 13:08:07 +0200

    ..

--- js/investments current.js
+++ js/investments current.js
@@ -23,27 +23,17 @@ function (data)
             for (var rows = $tbody.querySelectorAll('tr.m-loan-entry'), i = 0; i < rows.length - 1; i++)
             {
                 var cells   = rows[i].querySelectorAll('td');
+
+                if (cells[5].innerText === '-' || cells[5].innerText.split(' ')[1] === 'days')
+                {
+                    continue;
+                }
+
                 var days    = Math.floor(Math.abs((toDate(cells[5].innerText).getTime() - new Date().getTime()) / 86400000));
                 var node    = getElementByAttribute(cells, "data-m-label", 'Next Payment Date');
 
-                console.log(days)
                 node.innerText = days + ' days';
             }
         });
-
-        // 1: Register a dom listener (see finished investments)
-        // 2: Get reference to all rows
-        //  - See an example of how a row like this looks like in the button.
-        // 3: Get the cell with the 'data-m-label' value of of 'Next Payment Date' using:
-        //  getElementByAttribute (elements, attribute, value)
-        // like: var cell = getElementByAttribute(row, 'data-m-label', 'Next Payment Date')
-        // 4: Read the innerText
-        // - Maybe it's just a '-' ? see the screenshot. In this case, we don't wanna do anything. The '-' appear when a loan is late.
-        // - Maybe it's a date as: '10.09.2019'
-        //  In this case, calculate it to a date, and calculate the time in days between this string (as a date), and now.
-        // see this code for an example of how to calculate days between two dates:
-        // var days    = Math.floor(Math.abs((toDate(DATE 1 HERE AS STRING).getTime() - toDate(DATE 2 HERE AS STRING).getTime()) / 86400000));
-        // if either of them is already a date type, skip the toDate call.
-        // write the value days + ' days' to innerText
     }
 });
