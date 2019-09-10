3a4f5ef7c4fc7843a7877950345ac1b3c253ae94
DeeNaxic <DeeNaxic@gmail.com>
2019-09-10 11:47:40 +0200

    ..

--- js/overview.js
+++ js/overview.js
@@ -1,3 +1,4 @@
+
 /*
  *  @project >> Mintos Extension
  *  @version >> 1.0.0
@@ -25,10 +26,14 @@ chrome.storage.sync.get(
 },
 function (data)
 {
+    /*
+     *  This goes through all of the four boxes, on the overview page, including
+     *  the initially hidden one. It then iterate through all rows, in the boxes
+     *  and picks the value column. If this value when cast to a float, and then
+     *  to a string is zero, an inline display style of none is added to hide it
+     */
     if (data.OverviewHideZeroes)
     {
-        // Iterate through all rows in all boxes, and hide unused rows:
-        
         for (var i = 0; i < $boxes.length; i++)
         {
             for (var rows = $boxes[i].querySelector('tbody').querySelectorAll('tr'), j = 0; j < rows.length - 1; j++)
@@ -41,10 +46,15 @@ function (data)
         }
     }
     
+    /*
+     *  This will insert a third column to all tables where the percentage makes
+     *  sense. This includes the balance box, and the investment summaries boxes
+     *  It declares a local function, which can be used to insert the cell, with
+     *  two floats, the current number and the total amount. This will calculate
+     *  percentage automatically. This also handles switching the existing style
+     */
     if (data.OverviewShowPercentages)
     {
-        // Function to create and insert a percentage cell at the end of a row:
-        
         function insertPercentageCell (original, total)
         {
             var percent         = toFloat(original.innerText) / total * 100.00;
@@ -54,8 +64,6 @@ function (data)
             original.setAttribute('style', 'text-align:right;');
             original.parentNode.insertBefore(node, original.nextSibling);
         }
-        
-        // Iterate all boxes which needs to have percentage cells added to them:
         
         for (var rows = $boxBalance.querySelector('tbody').querySelectorAll('tr'), i = 0; i < rows.length; i++)
         {
@@ -73,21 +81,22 @@ function (data)
         }
     }
     
+    /*
+     *  This is a purely cosmetic change, which doesn't do anything. It replaces
+     *  the two raido button, for switching between displayed loans, to the type
+     *  of button used in the other two columns. It just hides the original ones
+     *  and then registers an event listener, on the new button, whitch switches
+     *  between clicking on either of the radio buttons in a very cruede fashion
+     */
     if (data.OverviewShowButtonInstead)
     {
-        // This function will toggle between the hidden original radioboxes:
-        
         function toggle ()
         {
             document.querySelector('.radios').querySelectorAll('label')[$toggle == 1 ? ($toggle = 0) : ($toggle = 1)].click();
         }
         
-        // Hide the original radio buttons:
-        
         document.querySelectorAll('.radios label')[0].style.display = 'none';
         document.querySelectorAll('.radios label')[1].style.display = 'none';
-        
-        // Insert a new button with the same style as the other buttons:
         
         var nodeOuter = document.createElement('div');
             nodeOuter.classList.add('btn-container');
@@ -103,10 +112,14 @@ function (data)
         document.querySelector('.radios').appendChild(nodeOuter);
     }
     
+    /*
+     *  This will add an inline style, of color red to any number which is below
+     *  zero. It does this by iterating all rows, in the returns box, and taking
+     *  the second cell in each of them, which is the value that hold the number
+     *  and then checks the value. Note that it only checks the box returns rows
+     */
     if (data.OverviewNegativeNumbers)
     {
-        // Iterate all rows in the return box, and color negative numbers red:
-        
         for (var rows = $boxReturns.querySelectorAll('tr'), i = 0; i < rows.length; i++)
         {
             if (toFloat(rows[i].querySelectorAll('td')[1].innerText) < 0.00)
