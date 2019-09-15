646d494e3744588e7ac77b7ca52b8bfaf42bb2ed
DeeNaxic <DeeNaxic@gmail.com>
2019-09-15 23:01:22 +0200

    Added global variables for tables.

--- scripts/loan.js
+++ scripts/loan.js
@@ -11,10 +11,15 @@ chrome.storage.sync.get(
 },
 function (data)
 {
-    if (document.location.pathname.match(/^\/\w{2}\/\d+-\d+/g) === null)
+    if (document.location.pathname.match(/^\/\w{2}\/[0-9]+-[0-9]+/g) === null)
     {
         return;
     }
+    
+    var $tables         = document.querySelectorAll('tbody');
+    var $details        = $tables[0];
+    var $borrower       = $tables[2];
+    var $schedule       = $tables[3];
     
     console.log("running the loan page extensions.");
 });
