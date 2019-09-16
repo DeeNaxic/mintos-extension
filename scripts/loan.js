9f5a9ec87069188fc12e9a3de7453d351b558ce4
DeeNaxic <DeeNaxic@gmail.com>
2019-09-16 19:44:59 +0200

    Added more template for loan page.

--- scripts/loan.js
+++ scripts/loan.js
@@ -18,8 +18,74 @@ function (data)
     
     var $tables         = document.querySelectorAll('tbody');
     var $details        = $tables[0];
-    var $borrower       = $tables[2];
-    var $schedule       = $tables[3];
+    var $borrower       = $tables[1];
+    var $schedule       = $tables[2];
     
-    console.log("running the loan page extensions.");
+    function createDetailsRow (header, content)
+    {
+        var nodeOuter = document.createElement('tr');
+            
+        var nodeInner = document.createElement('td');
+            nodeInner.innerHTML = header;
+            nodeInner.classList.add('field-description');
+            nodeOuter.appendChild(nodeInner);
+            
+        var nodeInner = document.createElement('td');
+            nodeInner.innerHTML = content;
+            nodeInner.classList.add('value');
+            nodeOuter.appendChild(nodeInner);
+            
+        return nodeOuter;
+    }
+    
+    /*
+     *  Experimental
+     */
+    if (true)
+    {
+        var tt = createDetailsRow('Country', document.querySelector('.m-h1 img').title);
+        $details.insertBefore(tt, $details.firstChild);
+    }
+    
+    /*
+     *  Experimental
+     */
+    if (true)
+    {
+        if ($details.lastChild.lastChild.innerText != 'Current')
+        {
+            $details.lastChild.style.background = '#d4574e22';
+        }
+    }
+    
+    /*
+     *  Experimental
+     */
+    if (true)
+    {
+        var paid = 0;
+        var late = 0;
+        
+        $schedule.querySelectorAll('tr').forEach(function (e)
+        {
+            if (e.lastChild.innerText == 'Paid')
+            {
+                paid++;
+            }
+            else
+            if (e.lastChild.innerText == 'Scheduled')
+            {
+                
+            }
+            else
+            {
+                late++;
+            }
+        });
+        
+        var percent = ((paid / (paid + late) * 100.00) || 100.00).toFixed(2) + '%';
+        var node = createDetailsRow('Perfect Payments', percent);
+        $details.appendChild(node);
+        console.log("On time payments: " + paid + " out of " + (paid + late) + " payments.");
+    }
 });
